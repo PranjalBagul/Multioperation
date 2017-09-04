cfa3ac740aab8ad58e4d7909f7dd49e66730e965
Student P_08 <studentp_08@Student-P-08.local>
2017-09-04 12:53:45 +0530

    Initial Commit

---
+++ multiOperation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  multiOperation
+//
+//  Created by Student P_08 on 04/09/17.
+//  Copyright © 2017 pranjal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ multiOperation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  multiOperation
+//
+//  Created by Student P_08 on 04/09/17.
+//  Copyright © 2017 pranjal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
